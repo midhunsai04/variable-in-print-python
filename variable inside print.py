13041c56215b0c3378e51c715e44abacb2d80e71
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-06 21:34:28 +0530

    Add files via upload

---
+++ variable inside print.py
@@ -0,0 +1,9 @@
+myname=input("what's your name?: ")
+myfav=input("what's your favourite food?: ")
+mymuz=input("what's your favourite music?: ")
+myplace=input("where do you live?: ")
+print()
+print(f"""you are {myname}
+you're probably hungry for {myfav}
+and you're definitely getting your groove on to {mymuz}
+living in the amazing {myplace}""")
